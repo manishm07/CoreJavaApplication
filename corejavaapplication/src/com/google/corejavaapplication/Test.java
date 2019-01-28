b1315cd20caba795a1ef6e62cb4e022c119e9b77
manishm07 <41923138+manishm07@users.noreply.github.com>
2019-01-28 20:10:28 +0545

    Add files via upload

---
+++ corejavaapplication/src/com/google/corejavaapplication/Test.java
@@ -0,0 +1,15 @@
+package com.google.corejavaapplication;
+
+
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is a test program:");
+        System.out.println("hello world!!" );
+
+    }
+
+
+}
